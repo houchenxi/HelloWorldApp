e20c8c34b485da1e3e1936ef540b304086eba0e0
houchenxi <495838087@qq.com>
2014-12-21 20:25:20 +0800

    【给Label新增输出口】

--- HelloWorldApp/ViewController.h
+++ HelloWorldApp/ViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *m_oButtonDisplay;
 
 - (IBAction)m_aButtonPressed:(UIButton *)sender;
 
